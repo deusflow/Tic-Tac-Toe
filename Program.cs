8fa5041759b3d66fabb0b578bd7be6f499a0135f
DeusFlow <deuswork@icloud.com>
2025-04-28 11:04:48 +0200

    start

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
-﻿namespace TicTacToe;
+﻿using System.Threading.Channels;
 
-class Program
+namespace TicTacToe;
+
+class TicTocToe
 {
-    static void Main(string[] args)
+    static char[] board = {'1','2','3','4','5','6','7','8','9'};
+    public static char currentPlayer;
+    
+    void showBoard()
     {
-        Console.WriteLine("Hello, World!");
+        //første række, felter 0, 1, 2 adskilt af lodrette  streger
+        Console.WriteLine($" {board[0]} | {board[1]} | {board[2]}");
+            Console.Write("___+___+___"); //separator
+        //anden række
+        Console.WriteLine($" {board[3]} | {board[4]} | {board[5]}");
+            Console.Write("___+___+___");
+        //tredje r.
+        Console.WriteLine($" {board[6]}, {board[7]}, {board[8]}");
     }
+
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Tic-Tac-Toe! Who starts this game? \n Enter X or 0:");
+        currentPlayer = Console.ReadLine().ToUpper()[0];
+    }
+
+
 }
